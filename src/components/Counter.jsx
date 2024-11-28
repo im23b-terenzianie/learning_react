fad7d66499cb04200616e3ac5792a1e8126e4d47
im23b-terenzianie <terenzianie@bzz.ch>
2024-11-28 15:46:52 +0100

    Initial commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+export default function Counter() {
+    const [counter, setCounter] = useState(0);
+
+
+    function countUp() {
+        setCounter()
+
+        return (
+            <div className="counter">
+                Der aktuelle Wert ist: {counter}
+                <button onClick={countUp}>Count up</button>
+                </div>
+        );
+    }
+}
