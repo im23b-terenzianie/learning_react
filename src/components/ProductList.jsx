41f6fae54dc5ccef1173656341ae694162fc74f7
im23b-terenzianie <terenzianie@bzz.ch>
2024-12-05 15:45:24 +0100

    Buttons ein wenig gestylte

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,7 +5,7 @@ export default function ProductList() {
     const limit = 12
     const [page, setPage] = useState(1)
     const [products, setProducts] = useState([])
-    let skip= limit*(page-1)
+    let skip = limit * (page - 1)
     useEffect(() => {
         fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
             .then(res => res.json())
@@ -24,9 +24,11 @@ export default function ProductList() {
 
             ))}
             {/* BUTTON:PREV ZAHL:CURRENT BUTTON:NEXT*/}
-            <button className="border-1" onClick={() =>setPage(page-1)} disabled={page=== 1}>Prev</button>
-            <p className="mx-2">{page}</p>
-            <button className="border-1" onClick={() =>setPage(page+1)}>Next</button>
+            <div className="w-full flex justify-end ">
+                <button className="hover:underline" onClick={() => setPage(page - 1)} disabled={page === 1}>Prev</button>
+                <p className="mx-3">{page}</p>
+                <button className="hover:underline" onClick={() => setPage(page + 1)}>Next</button>
+            </div>
         </div>
     )
 }
