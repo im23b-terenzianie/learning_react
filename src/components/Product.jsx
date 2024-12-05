c18cea7e322adc65ad9d4ad17bbc86b3ad8ca20b
im23b-terenzianie <terenzianie@bzz.ch>
2024-12-05 13:26:17 +0100

    Productlist and Product verbessert und einbindung von Tailwind

---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+export default function Product(){
+    return(
+        <div className="w-1/4 p-5">
+            <img src="https://placehold.it/800"/>
+            <h2 className="font-bold">Product Title</h2>
+            <p>Product Description</p>
+            <p className="text-gray-500">Product Price</p>
+        </div>
+    )
+}
