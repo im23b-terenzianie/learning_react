c98eb1f927bceb1f4e1fbd9ef41afe7f9d494361
im23b-terenzianie <terenzianie@bzz.ch>
2024-12-05 15:30:10 +0100

    Mehrere Pages hinzugefügt

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,15 +2,18 @@ import Product from "./Product";
 import {useState, useEffect} from "react";
 
 export default function ProductList() {
+    const limit = 12
+    const [page, setPage] = useState(1)
     const [products, setProducts] = useState([])
+    let skip= limit*(page-1)
     useEffect(() => {
-        fetch('https://dummyjson.com/products')
+        fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
             .then(res => res.json())
             .then(data => {
                 console.log(data)
                 setProducts(data.products)
             })
-    }, []);
+    }, [page]);
     return (
         <div className="flex flex-wrap w-full p-5">
             {products && products.map((product) => (
@@ -20,6 +23,10 @@ export default function ProductList() {
                 />
 
             ))}
+            {/* BUTTON:PREV ZAHL:CURRENT BUTTON:NEXT*/}
+            <button className="border-1" onClick={() =>setPage(page-1)} disabled={page=== 1}>Prev</button>
+            <p className="mx-2">{page}</p>
+            <button className="border-1" onClick={() =>setPage(page+1)}>Next</button>
         </div>
     )
 }
