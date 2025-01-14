99d0da4f1eb7a1e9683fbc5a882a540657d69021
im23b-terenzianie <terenzianie@bzz.ch>
2025-01-14 14:41:50 +0100

    routing funktioniert

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,7 @@ export default function Product(props) {
     const product = props.product
     const navigate = useNavigate()
     return (
-        <div className="w-1/4 p-5" onClick={()=>{navigate(`/products/:${product.id}`)}}>
+        <div className="w-1/4 p-5" onClick={()=>{navigate(`/products/${product.id}`)}}>
             <img src={product.thumbnail}/>
             <h2 className="font-bold">{product.title}</h2>
             <p>{product.description}</p>
--- src/pages/Productdetails.jsx
+++ src/pages/Productdetails.jsx
@@ -1,8 +1,32 @@
 
+import Base from "../layout/Base";
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
 export default function Productdetails() {
+    const id = useParams().id
+    const [product, setProduct] = useState(null )
+    useEffect(() => {
+        fetch(`https://dummyjson.com/products/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setProduct(data)
+                console.log(data)
+            })
+    }, [id])
+    if (!product) {
+        return <div>Loading...</div>
+    }
     return (
         <div>
-            <h1>Productdetails</h1>
+            <Base>
+                <div className="">
+                    <h1 className="font-bold text-2xl ">{product.title}</h1>
+                    <img src={product.thumbnail}/>
+                    <p>{product.description}</p>
+                    <p className="text-gray-500">{product.price}</p>
+                </div>
+            </Base>
         </div>
     )
 }
